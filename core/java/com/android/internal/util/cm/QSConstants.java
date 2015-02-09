6e38b7e59a7e97314dfca751fa3ea81a1221f78b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:39:52 +0000

    QS: add Battery saver tile
    PureNexus Edits:
    * adapt for marshmallow
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -44,6 +44,7 @@ public class QSConstants {
     public static final String TILE_NFC = "nfc";
     public static final String TILE_COMPASS = "compass";
     public static final String TILE_SYNC = "sync";
+    public static final String TILE_BATTERY_SAVER = "battery_saver";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -78,5 +79,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_NFC);
         TILES_AVAILABLE.add(TILE_COMPASS);
         TILES_AVAILABLE.add(TILE_SYNC);
+        TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
     }
 }
