f5958b7337a6a9a2cd49e072dc2b3cedc7f11305
Noeljunior <liamgliam@gmail.com>
2015-04-26 11:46:43 +0100

    QS: Add Ambient Display Tile (1/2)
    PureNexus Edits:
    *adapt for marshmallow
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -45,6 +45,7 @@ public class QSConstants {
     public static final String TILE_COMPASS = "compass";
     public static final String TILE_SYNC = "sync";
     public static final String TILE_BATTERY_SAVER = "battery_saver";
+    public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -80,5 +81,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_COMPASS);
         TILES_AVAILABLE.add(TILE_SYNC);
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
+        TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
     }
 }
