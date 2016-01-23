a72aa52e340b5248a4ec35a975dc175a1aad0fc6
Alex Cruz <mazdarider23@gmail.com>
2016-01-22 22:31:00 -0500

    Dashboard tileview double lines toggle [2/2]
    Change-Id: I06d5d1f4fd6f9de123ac72635c4d76973a2aec4d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4197,6 +4197,12 @@ public final class Settings {
         public static final String DASHBOARD_COLUMNS = "dashboard_columns";
 
         /**
+         * Whether to display dashboard tiles in a double line layout
+         * @hide
+         */
+        public static final String DASHBOARD_TILEVIEW_DOUBLE_LINES = "dashboard_tileview_double_lines";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
