ba4d11d20b4e4112da730ef4a5df6f97b865e0f2
Alex Cruz <mazdarider23@gmail.com>
2016-01-22 22:32:05 -0500

    Configurable dashboard columns [2/2]
    Change-Id: Icb79592201ed3f8fa097ab3fb9e919dd98b556d1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4191,6 +4191,12 @@ public final class Settings {
         public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
 
         /**
+         * Change the number of columns displayed in the Settings dashboard
+         * @hide
+         */
+        public static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
