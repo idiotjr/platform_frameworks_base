c47784bcfedda261885bce6d0fff06214bc59463
Randall Rushing <randall.rushing@gmail.com>
2016-08-10 00:44:27 -0700

    Fling: Trails width configuration [2/3]
    Change-Id: I0a48b554b36c23691b2fe7cac933975bf6ddc6b5
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6916,7 +6916,14 @@ public final class Settings {
          */
         public static final String FLING_LONGSWIPE_THRESHOLD_DOWN_LAND = "fling_longswipe_threshold_down_land";
 
-         /**
+        /**
+         * width of Fling trails stroke, in density pixels
+         *
+         * @hide
+         */
+        public static final String FLING_TRAILS_WIDTH = "fling_trails_width";
+
+        /**
          * Whether user is allowed to pull down quick settings on secure keyguard.
          * @hide
          */
