0a3e4b68d2a18405b34fd9d9bf3a8eecb5b7cbcd
srisurya95 <srisurya95@gmail.com>
2016-11-10 10:43:16 -0500

    Revert OMS3 [SQUASH]
    Revert "Small adjustment to Font commit"
    
    This reverts commit ebc51e307820c4f580e0eea777a29da9a6020f76.
    
    Revert "N-Extras: Add dynamic theme fonts support (Squash)"
    
    This reverts commit eeb5e3cd1b48f16a3279c68f2665b4ec1e32ecc1.
    
    Revert "OMS-N: Themes should be dynamically theming the power menu"
    
    This reverts commit 8d8147188582f29f1ddcd45fa5110dbe2d3047da.
    
    Revert "N-Extras: AudioService: Allow system effect sounds to be themed"
    
    This reverts commit 149f8f1b55bbb5453f3091fc57be8f657da482cb.
    
    Revert "N-Extras: Add dynamic theme BootAnimation support"
    
    This reverts commit f359a42d9e9e1ee4cb8b374e6cdfc8a35b684537.
    
    Revert "OMS-N: Disable Zygote preloaded drawables."
    
    This reverts commit 992f62898b9383645c023446916fe42ab669f2d2.
    
    Revert "OMS-N: Allow on-demand window refreshes using "om refresh""
    
    This reverts commit 4083d583ec9764e33a08a9e5c0ca6fd55687a659.
    
    Revert "OMS-N: Allow multiple targets for enabling and disabling overlays"
    
    This reverts commit a0b95d7613eb62279eb63d136c09bed4f639daf7.
    
    Revert "OMS-N: Officially announce the rebase completion to Android Nougat [14/14]"
    
    This reverts commit 50c9351cbf4400fb9bb7f3f24311e10f0cc3dec7.
    
    Revert "OMS-N: adb shell command to access OverlayManagerService [13/14]"
    
    This reverts commit 961a0d41b16d86bfb1625f35b0702b988aaa4466.
    
    Revert "OMS-N: Ensure unique type IDs when parsing overlays [12/14]"
    
    This reverts commit 427f2d177e4ce6136d2558ec4a43865a51b26f58.
    
    Revert "OMS-N: Persistence on boot through OverlayManagerServiceImpl [11/14]"
    
    This reverts commit 8050ed3a41db8c9b65515cedfe45787257c1fe0a.
    
    Revert "OMS-N: Fix memory leak during idmap creation [10/14]"
    
    This reverts commit 0f9af7bcc3bbc034c0ad52534516aee8ff399848.
    
    Revert "OMS-N: Check invariants after ResTable is modified [9/14]"
    
    This reverts commit 88bbeb27ce037d2636be183f5fd8fedb0f941bba.
    
    Revert "OMS-N: idmap: suppress print for padded resources [8/14]"
    
    This reverts commit 41cb049f54aa127cf81d5e15b41ff030e52978da.
    
    Revert "OMS-N: pm: set EXTRA_REPLACING correctly in ACTION_PACKAGE_ADDED [7/14]"
    
    This reverts commit 778cf6606979952f73744b5d5bfbea19b8da5a9e.
    
    Revert "OMS-N: integrate OverlayManagerService into framework [6/14]"
    
    This reverts commit 0cb14a100d0ccf187890dde8e37adca162b6a7f7.
    
    Revert "OMS-N: introduce the OverlayManagerService [5/14]"
    
    This reverts commit ca20aa24983076f96fe46b4baab968ffa7548a17.
    
    Revert "OMS-N: Add ResTable::remove(cookie) [4/14]"
    
    This reverts commit 10294c8d8235e3dbb7401376294c1e1d2f65b156.
    
    Revert "OMS-N: Make asset manager cookies opaque [3/14]"
    
    This reverts commit 1463d7c509814fdfc49f595ff13af28cbff03d6b.
    
    Revert "OMS-N: installd: add command 'rmidmap' [2/14]"
    
    This reverts commit a2e279af5d2ccd498aa5a928644fa0ca09398e4d.
    
    Revert "OMS-N: Support downloaded overlay packages [1/14]"
    
    This reverts commit f7a5f69c7600837e57d2396d87688dca68ee8cd4.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -52,7 +52,6 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.widget.ListView;
 
-import com.android.internal.util.ThemeUtils;
 import com.android.internal.telephony.ITelephony;
 import com.android.server.pm.PackageManagerService;
 
@@ -138,7 +137,7 @@ public final class ShutdownThread extends Thread {
         mReboot = false;
         mRebootSafeMode = false;
         mReason = reason;
-        shutdownInner(getUiContext(context), confirm);
+        shutdownInner(context, confirm);
     }
 
     private static boolean isAdvancedRebootPossible(final Context context) {
@@ -184,12 +183,12 @@ public final class ShutdownThread extends Thread {
         if (confirm) {
             final CloseDialogReceiver closer = new CloseDialogReceiver(context);
             final boolean advancedReboot = isAdvancedRebootPossible(context);
-            final Context mUiContext = getUiContext(context);
+
             if (sConfirmDialog != null) {
                 sConfirmDialog.dismiss();
                 sConfirmDialog = null;
             }
-            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(mUiContext)
+            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(context)
                     .setTitle(mRebootSafeMode
                             ? com.android.internal.R.string.reboot_safemode_title
                             : showRebootOption
@@ -289,7 +288,7 @@ public final class ShutdownThread extends Thread {
         mRebootSafeMode = false;
         mRebootHasProgressBar = false;
         mReason = reason;
-        shutdownInner(getUiContext(context), confirm);
+        shutdownInner(context, confirm);
     }
 
     /**
@@ -805,12 +804,5 @@ public final class ShutdownThread extends Thread {
         if (!done[0]) {
             Log.w(TAG, "Timed out waiting for uncrypt.");
         }
-    }
-
-    private static Context getUiContext(Context context) {
-        Context mUiContext = null;
-        mUiContext = ThemeUtils.createUiContext(context);
-        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
-        return mUiContext != null ? mUiContext : context;
     }
 }
